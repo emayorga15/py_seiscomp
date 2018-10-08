05a6f14b4140b3479f53a4d79f1bd9416cf99622
dsiervo <emetdan@gmail.com>
2018-10-08 09:34:48 +0000

    Se sube script que convierte eventos de seiscomp en sfiles

---
+++ seiscomp2sfiles.py
@@ -0,0 +1,53 @@
+"""
+Daniel Siervo <dsiervo@sgc.gov.co>
+"""
+
+from obspy import UTCDateTime
+from obspy.clients.fdsn import Client
+from obspy.core.event import ResourceIdentifier
+import numpy as np
+import os
+
+server_IP = 'http://10.100.100.232'
+port_fdsn = "8091"
+    
+
+starttime = UTCDateTime("2018 08 14")    
+
+endtime = UTCDateTime("2018 09 04")
+
+client = Client(server_IP+":"+port_fdsn)
+
+# catalogo de eventos correspondiente al select
+cat = client.get_events(starttime=starttime, endtime=endtime,
+                        minlatitude=10, maxlatitude=14,
+                        minlongitude=-82, maxlongitude=-80)
+
+print len(cat)
+
+folder_name = "PRV_sfiles"
+os.system("mkdir %s"%folder_name)
+for event in cat:
+    try:
+        #print event
+        time = event.preferred_origin().time
+        # la forma de onda empieza 2 min antes del sismo
+        min_w = time.minute - 2
+        event_id = event.resource_id.resource_id.split('/')[2]
+        print event_id
+        # "ARC PRV   HHZ CM 00 2018 0814 1418 03   300"
+        wz ="ARC PRV   HHZ CM 00 %s %s%s %s%s %s   300"%(\
+            str(time.year), str(time.month).rjust(2,"0"), str(time.day).rjust(2,"0"),
+            str(time.hour).rjust(2,"0"), str(min_w).rjust(2,"0"), str(time.second).rjust(2,"0"))
+        wn = wz.replace("HHZ", "HHN")
+        we = wz.replace("HHZ", "HHE")
+        #print w
+        name = str(time.day).rjust(2,"0")+"-"+str(time.hour).rjust(2,"0")+str(time.minute).rjust(2,"0")+\
+        "-"+str(time.second).rjust(2,"0")+"L.S"+str(time.year)+str(time.month).rjust(2,"0")
+        event.write(folder_name+"/"+name, format="NORDIC", userid="anls",
+                   wavefiles=[wz])
+        print 'Output file name: ' +  name
+    except Exception as e:
+        print e
+        print name
+        continue
